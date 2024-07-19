fd14e45319b26d9fe570980195f1ef61c06c5103
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-18 21:46:41 -0300

    ✨ feat: create verifyuserauth middleware

---
+++ api/src/middlewares/verifyUserAuthorization.js
@@ -0,0 +1,11 @@
+function VerifyUserAuthorization(roleToVerify){
+    return (request, response, next) => {
+        const { role } = request.user
+        if(role !== roleToVerify){
+            throw new AppError('Unauthorized', 401)
+        }
+        return next();
+    }
+}
+
+module.exports = VerifyUserAuthorization;
