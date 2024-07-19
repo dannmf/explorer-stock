6d56e96a9155121e8878585fd8115dfc5b86ed1e
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-19 19:56:59 -0300

    ✨ feat: validate users unauthorizeds

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
+const UsersValidatedController = require("../controllers/UsersValidatedController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const usersValidatedController = new UsersValdatedController();
+
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.get("/validated", ensureAuthenticated, usersValidatedController.index);
+
 
 module.exports = usersRoutes;
