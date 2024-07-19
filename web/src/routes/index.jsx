3597525f948a9fc9b6c86d46d134d9e0b4157611
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-18 22:11:29 -0300

    ♻️ refactor: separating routes by roles

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
 
-import { AppRoutes } from './app.routes';
+import { AppRoutes } from './admin.routes';
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
