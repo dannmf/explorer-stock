3dc84b6df4ea8ef3b40233f9aad51a83fa88a868
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-19 19:39:11 -0300

    ♻️ refactor: passing cookie for authorizations

--- web/src/pages/SalesReport/index.jsx
+++ web/src/pages/SalesReport/index.jsx
@@ -1,6 +1,10 @@
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function SalesReport() {
+  useEffect(() => {
+    api.get('/sales', { withCredentials: true })
+  }, [])
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
